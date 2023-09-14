7fb23b0505638e8f1dc2e0cb22616686c3d12d7f
Orlando Diaz <orlandoddiaz@unicesar.edu.co>
2023-09-14 01:23:25 -0500

    project separation in modal

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-
 document.getElementById("simpleInterestBtn").addEventListener("click", function() {
     const principal = parseFloat(document.getElementById("principal").value);
     const rate = parseFloat(document.getElementById("rate").value);
     const time = parseFloat(document.getElementById("time").value);
-
-    const simpleInterest = (principal * rate * time) / 100;
-    document.getElementById("result").innerHTML = `El interés simple es: $${simpleInterest.toFixed(2)}`;
+    if ((isNaN(principal) || principal === 0) && (isNaN(rate) || rate === 0) && (isNaN(time) || time === 0)) {
+        document.getElementById("myModal").style.display = "flex";
+    } else {
+        const simpleInterest = (principal * rate * time) / 100;
+        document.getElementById("result").innerHTML = `El interés simple es: $${simpleInterest.toFixed(2)}`;
+    }
 });
 
 document.getElementById("compoundInterestBtn").addEventListener("click", function() {
     const principal = parseFloat(document.getElementById("principal").value);
     const rate = parseFloat(document.getElementById("rate").value);
     const time = parseFloat(document.getElementById("time").value);
+    if ((isNaN(principal) || principal === 0) && (isNaN(rate) || rate === 0) && (isNaN(time) || time === 0)) {
+        document.getElementById("myModal").style.display = "flex";
+    } else {
+        const compoundInterest = principal * (Math.pow(1 + rate / 100, time) - 1);
+        document.getElementById("result").innerHTML = `El interés compuesto es: $${compoundInterest.toFixed(2)}`;
+    }
+});
 
-    const compoundInterest = principal * (Math.pow(1 + rate / 100, time) - 1);
-    document.getElementById("result").innerHTML = `El interés compuesto es: $${compoundInterest.toFixed(2)}`;
+document.getElementById("closeModalBtn").addEventListener("click", function() {
+    document.getElementById("myModal").style.display = "none";
 });
